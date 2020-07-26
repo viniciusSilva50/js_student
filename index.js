ef1da0829b2761ec441f7185f7712cd4b46b4155
vinicius <viniciusdasilva50@gmail.com>
2020-07-25 23:58:22 -0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const operators = require("./modules/operators")
 
-console.log("Running")
+const { promiseAllTest } = require('./modules/promises')
 
-const runOperators = () => {
-    console.log('Run operators')
-    operators._comma()
-    operators._deleteInArray()
-    operators._deleteInObject()
-    operators._in()
-    operators._spreadArray()
-    operators._spreadObject()
-
+const runPromises = async () => {
+  console.log('Start run promises')
+  await promiseAllTest()
+  console.log('End run promises')
 }
 
 const run = () => {
-    runOperators()
+  runPromises()
 }
 
 run()
